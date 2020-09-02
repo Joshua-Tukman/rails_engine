bbd4be9ea027ed991809b29e60e704feb2d81a24
Josh Tukman <josh.tukman@gmail.com>
2020-09-02 01:05:55 -0600

    Add new routes for merchant_items

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items
-      resources :merchants
+      resources :items, except: [:new, :edit] do
+        get '/merchant', to: 'merchant_items#show'
+      end 
+      resources :merchants, except: [:new, :edit] do
+        get '/items', to: 'merchant_items#index'
+      end 
     end
   end
 end
