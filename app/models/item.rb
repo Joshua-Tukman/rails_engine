8f70df1013218b8d45b81a3d4746b7a8628dba96
Josh Tukman <josh.tukman@gmail.com>
2020-09-03 00:43:04 -0600

    Add class AR methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.find_names(fragment)
+    Item.where('LOWER(items.name) LIKE ?', fragment)
+  end
+
+  def self.find_one(fragment)
+    item = Item.find_by('LOWER(items.name) LIKE ?', fragment)
+  end
 end
